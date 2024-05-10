2a2a7ad3c101f5918dfaba3e353d0c0abca9cd78
aldse <eliana.tete1211@gmail.com>
2024-05-10 11:09:46 -0300

    try

---
+++ Back/DataBase.sql
@@ -0,0 +1,41 @@
+create database if not exists JobMatch;
+
+use JobMatch;
+
+create table Endereco (
+	IDEndereco int primary key auto_increment,
+    Pais varchar(20) not null,
+    UF char(2) not null,
+    Municipio varchar(30) not null,
+    Cep varchar(9) not null,
+    Bairro varchar(30) not null,
+    Logradouro varchar(30) not null,
+    Complemento varchar(20) not null
+);
+
+create table Usuario (
+	IDUsuario int primary key auto_increment,
+    Nome varchar(60) not null,
+    Email varchar(50) not null,
+    Cpf varchar(11) not null,
+    Senha varchar(50) not null,
+    DataNascimento int not null,
+    IDEndereco int,
+    foreign key (IDEndereco) references Endereco(IDEndereco)
+);
+
+create table Taxa (
+	IDTaxa int primary key auto_increment,
+    Titulo varchar(30) not null,
+    Descricao varchar(255) not null,
+    DataInicio int not null,
+    DataTermino int,
+    Valor double not null,
+    QtdTaxa tinyint not null,
+    IDUsuario int not null,
+    IDEndereco int,
+    foreign key (IDUsuario) references Usuario(IDUsuario),
+    foreign key (IDEndereco) references Endereco(IDEndereco)
+);
+
+# drop database FreeLance;
--- Front/src/components/LoginComponent/index.jsx
+++ Front/src/components/LoginComponent/index.jsx
@@ -1,28 +1,50 @@
+import React, { useState } from 'react';
+
 import styles from "./style.module.scss"
 import logo from "../../Img/logo.png"
-import user from "../../Img/user.png"
-function LoginComponent() {
-  return (
-    <>
-        <div className={styles.body}>
-                    <div className={styles.welcome}>
-                        <img src={logo} className={styles.logo}/>
-                        <h2> BEM VINDO</h2>
-                        <div className={styles.left}>
-                            <h1> Novo Login </h1>
-                            <hr className={styles.linha}/>
-                        </div>
-                        <button className={styles.button}> Criar Conta </button>
-                    </div>
+import eyeIconOpen from '../../Img/olhoAb.png';
+import eyeIconClose from '../../Img/olhoFec.png';
 
-                    <div className={styles.logar}>
-                        <h1> Faça o seu Login </h1>
-                        <input type="text" placeholder="CPF"/>
-                        <input type="password" placeholder="******"/>
+function LoginComponent() {
+    const [showPassword, setShowPassword] = useState(false);
+    const [inputType, setInputType] = useState('password');
+    const [eyeIconSrc, setEyeIconSrc] = useState(eyeIconClose);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+        setInputType(showPassword ? 'password' : 'text');
+        setEyeIconSrc(showPassword ? eyeIconClose : eyeIconOpen);
+    };
+    return (
+        <>
+            <div className={styles.body}>
+                <div className={styles.welcome}>
+                    <img src={logo} className={styles.logo} />
+                    <h2> BEM VINDO</h2>
+                    <div className={styles.left}>
+                        <h1> Novo Login </h1>
+                        <hr className={styles.linha} />
                     </div>
-        </div>
-    </>
-  );
+                    <button className={styles.button}> Criar Conta </button>
+                </div>
+
+                <div className={styles.logar}>
+                    <h1> Faça o seu Login </h1>
+                    <input type="text" placeholder="| CPF" className={styles.input1} />
+                    <div className={styles.inputDiv}>
+
+                        <input type={inputType} placeholder="| ******" className={styles.input2} />
+                        <img
+                            src={eyeIconSrc} 
+                            className={styles.eyeIcon}
+                            onClick={togglePasswordVisibility}
+                            />
+                    </div>
+                    <button className={styles.entrar}>Entrar</button>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default LoginComponent;
