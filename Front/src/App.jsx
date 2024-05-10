b5825f47c55f7f7763581190e28fbe5bff6670be
Emyli Quadros <babaloogames@gmail.com>
2024-05-10 09:58:13 -0300

    inicializando front login

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
 
@@ -9,7 +10,8 @@ function App() {
     <>
       <Router>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path='/' element={<Login />} />
+            <Route path="/Home" element={<Home />} />
           </Routes>
       </Router>
     </>
