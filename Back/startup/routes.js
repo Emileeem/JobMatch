f9de3131ce3f6ed45403562970fc5e36a9541914
Emyli Quadros <babaloogames@gmail.com>
2024-05-20 11:07:40 -0300

    aqui

--- Back/startup/routes.js
+++ Back/startup/routes.js
@@ -1,11 +1,16 @@
 import express from "express";
 import UserController from "../controller/userController.js"
 import TaxaController from "../controller/taxaController.js"
+import LoginController from "../controller/loginController.js"
 const routes = express.Router();
 
 // Users
 routes.get("/api/taxa/:id", UserController.getUserById)
 routes.post("/api/user/", UserController.create)
+
+//Login 
+.post('/api/login', LoginController.login)
+
 // routes.delete("/api/user/:id", UserController.delete)
 routes.put("/api/user/:id", UserController.update)
 
--- Front/src/components/LoginComponent/index.jsx
+++ Front/src/components/LoginComponent/index.jsx
@@ -4,6 +4,8 @@ import styles from "./style.module.scss"
 import logo from "../../Img/logo.png"
 import eyeIconOpen from '../../Img/olhoAb.png';
 import eyeIconClose from '../../Img/olhoFec.png';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 function LoginComponent() {
     const [showPassword, setShowPassword] = useState(false);
@@ -15,6 +17,39 @@ function LoginComponent() {
         setInputType(showPassword ? 'password' : 'text');
         setEyeIconSrc(showPassword ? eyeIconClose : eyeIconOpen);
     };
+
+    const [cpf, setCpf] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+    
+        if (!formValid()) return;
+    
+        const json = {
+            cpf,
+            password,
+        };
+    
+        console.log(json);
+    
+        try {
+            var res = await axios.post("http://localhost:3000/api/login", json);
+            console.log(res);
+            navigate("/home")
+        } catch (error){
+            console.log(error);
+        }
+    }
+    
+      function formValid() {
+        if (!cpf || !password) {
+          return false;
+        }
+        return true;
+      }
+
+      
     return (
         <div className={styles.background}>
             <div className={styles.body}>
@@ -30,17 +65,29 @@ function LoginComponent() {
 
                 <div className={styles.logar}>
                     <h1> Faça o seu Login </h1>
-                    <input type="number" placeholder="| CPF" className={styles.input1} />
+                    <input 
+                        type="number" 
+                        placeholder="| CPF" 
+                        className={styles.input1} 
+                        value={cpf}
+                        onChange={(e) => setCpf(e.target.value)}
+                    />
                     <div className={styles.inputDiv}>
 
-                        <input type={inputType} placeholder="| ******" className={styles.input2} />
+                        <input 
+                            type={inputType} 
+                            placeholder="| ******" 
+                            className={styles.input2}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                         />
                         <img
                             src={eyeIconSrc} 
                             className={styles.eyeIcon}
                             onClick={togglePasswordVisibility}
                             />
                     </div>
-                    <button className={styles.entrar}>Entrar</button>
+                    <button className={styles.entrar} onClick={handleSubmit}>Entrar</button>
                     <br/>
                     <b className={styles.cadastro2}>
                         Não tem uma conta?
