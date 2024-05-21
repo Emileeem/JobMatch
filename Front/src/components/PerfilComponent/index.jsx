57c8c6640f0b124faf31f34cf55196273c1b405c
Murilo Kauan Socek <socekmurilo@gmail.com>
2024-05-21 10:03:43 -0300

    Add struct Perfil

--- Front/src/components/PerfilComponent/index.jsx
+++ Front/src/components/PerfilComponent/index.jsx
@@ -1,9 +1,44 @@
 import styles from './styles.module.scss';
+import decoration1 from "../../assets/decoration1.png";
+import logo from "../../assets/logosemname.png";
+import Image from "react-bootstrap/Image";
 
 function PerfilComponent() {
     return (
         <>
-            
+            <div>
+                <Image className={styles.logo} src={logo} />
+                <Image className={styles.img} src={decoration1}/>
+                <div className={styles.title}>
+                    <h1>
+                        Editar Perfil
+                    </h1>
+                </div>
+                <div className={styles["wrap-inputs"]}>
+                    <div className={styles.inputs}>
+                        <input type="text" placeholder="| Nome Completo" className={styles.input} />
+                        <input type="date" placeholder="| DataNasc" className={styles.input} />
+                        <input type="text" placeholder="| Município" className={styles.input} />
+                        <input type="text" placeholder="| Complemento" className={styles.input} />
+                    </div>
+                    <div className={styles.inputs}>
+                        
+                        <input type="text" placeholder="| Sobrenome" className={styles.input} />
+                        <input type="text" placeholder="| País" className={styles.input} />
+                        <input type="number" placeholder="| CEP" className={styles.input} />
+                        <input type="password" placeholder="| Senha" className={styles.input} />
+                    </div>
+                    <div className={styles.inputs}>
+                    <input type="text" placeholder="| Email" className={styles.input} />
+                        <input type="text" placeholder="| Uf" className={styles.input} />
+                        <input type="text" placeholder="| Bairro" className={styles.input} />
+                        <input type="password" placeholder="| Confirmar Senha" className={styles.input} />
+                    </div>
+                </div>
+                <div className={styles.buttons}>
+                    <button className={styles.button}> Salvar Alterações </button>
+                </div>
+            </div>
         </>
     );
 }
