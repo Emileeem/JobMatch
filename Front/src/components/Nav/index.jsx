79e3d9f1277651ab5656b8b8e76842d8747f7a78
Emyli Quadros <babaloogames@gmail.com>
2024-05-22 10:50:21 -0300

    nav bar responsiva

--- Front/src/components/Nav/index.jsx
+++ Front/src/components/Nav/index.jsx
@@ -1,25 +1,30 @@
+import React, { useState } from 'react'; // Importe o useState
 import styles from './styles.module.scss';
-import Stack from 'react-bootstrap/Stack';
 import Image from "react-bootstrap/Image";
 import exit from "./exit.png";
-import add from "./add.png";
-import React from 'react';
 import { Link } from 'react-router-dom';
+import menu from "../../Img/menu.png";
 
 function Nav() {
+    const [isNavVisible, setIsNavVisible] = useState(false); // Estado para controlar a visibilidade do menu
+
+    const toggleNavVisibility = () => {
+        setIsNavVisible(!isNavVisible); // Alterna a visibilidade do menu
+    };
+
     return (
         <>
-            <div className={styles.navContainer}>
+            <div className={styles.navContainer} style={{ display: isNavVisible ? 'block' : 'none' }}>
                 <Link to="/vagas" className={styles.navItem}>Vagas</Link>
                 <Link to="/perfil" className={styles.navItem}>Perfil</Link>
                 <Link to="/taxasAgendadas" className={styles.navItem}>Taxas Agendadas</Link>
                 <Link to="/taxasConcluidas" className={styles.navItem}>Taxas Concluídas</Link>
                 <Link to="/adicionarVagas" className={styles.navItem}>Adicionar Vagas</Link>
-            {/* <Image className={styles.add} src={add} /> */}
             </div>
-
             <Image className={styles.exit} src={exit} />
+            <Image className={styles.menu} src={menu} onClick={toggleNavVisibility} />
         </>
     );
 }
+
 export default Nav;
