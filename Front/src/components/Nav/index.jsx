b31b7c1d42b83d351aad708a77a7dadaefdfc893
Emyli Quadros <babaloogames@gmail.com>
2024-05-22 11:19:41 -0300

    home responsive

--- Front/src/components/Nav/index.jsx
+++ Front/src/components/Nav/index.jsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react'; // Importe o useState
 import styles from './styles.module.scss';
 import Image from "react-bootstrap/Image";
-import exit from "./exit.png";
+import sair from "../../Img/exit.png";
 import { Link } from 'react-router-dom';
 import menu from "../../Img/menu.png";
+import { useNavigate } from "react-router-dom";
 
 function Nav() {
     const [isNavVisible, setIsNavVisible] = useState(false); // Estado para controlar a visibilidade do menu
+    const navigate = useNavigate(); 
 
     const toggleNavVisibility = () => {
         setIsNavVisible(!isNavVisible); // Alterna a visibilidade do menu
     };
 
+    const exit = () => {
+        navigate("/")
+    }
     return (
         <>
             <div className={styles.navContainer} style={{ display: isNavVisible ? 'block' : 'none' }}>
@@ -21,7 +26,7 @@ function Nav() {
                 <Link to="/taxasConcluidas" className={styles.navItem}>Taxas Concluídas</Link>
                 <Link to="/adicionarVagas" className={styles.navItem}>Adicionar Vagas</Link>
             </div>
-            <Image className={styles.exit} src={exit} />
+            <Image className={styles.exit} src={sair} onClick={exit}/>
             <Image className={styles.menu} src={menu} onClick={toggleNavVisibility} />
         </>
     );
